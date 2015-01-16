358b3184ae35490fbc2c27e6c3df8eba684a052b
Stuart Lees <stuartlees@gmail.com>
2015-01-16 17:49:13 -0500

    Overriding STORAGE_MODAL config value
    - Defining a different value to override the `STORAGE_MODAL` value pertaining to `storage-selector.config` module for dev and test environments
    - settings HTML updated to ensure `config.js` and settings app get imported and concatenated in the build after `storage-selector`

--- src/config/test.js
+++ src/config/test.js
@@ -9,5 +9,8 @@ if (typeof config === "undefined") {
     angular.module("risevision.common.i18n.config", [])
       .constant("LOCALES_PREFIX", "locales/translation_")
       .constant("LOCALES_SUFIX", ".json");
+
+    angular.module("risevision.widget.common.storage-selector.config")
+      .value("STORAGE_MODAL", "http://storage.risevision.com/~rvi/storage-client-rva-test/storage-modal.html#/files/");
   }
 }
