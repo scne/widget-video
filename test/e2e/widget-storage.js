49f397d3ee8e857beab85b7950e252fa3aa00537
Stuart Lees <stuartlees@gmail.com>
2015-02-10 15:17:47 -0500

    Separation of url and rise-storage testing
    - `gulpfile.js` revised to have two new widget e2e tasks that manually (no use of factory) generate `-e2e` files for url and storage so that they use their own mock data files
    - bower update to use latest `widget-settings-ui-components` release
    - adding `widget-storage.js` e2e test page with tests set to skip that pertain to the usage of rise-storage
    - updated `storage.js` mock data with different values

--- test/e2e/widget-storage.js
+++ test/e2e/widget-storage.js
@@ -13,11 +13,11 @@
 
   browser.driver.manage().window().setSize(1024, 768);
 
-  describe("Video Widget e2e Testing - via non-storage URL", function() {
+  describe("Video Widget e2e Testing - via storage URL", function() {
 
     beforeEach(function () {
       // point directly to the widget e2e file
-      browser.driver.get("http://localhost:8099/src/widget-url-e2e.html");
+      browser.driver.get("http://localhost:8099/src/widget-storage-e2e.html");
 
       // need to ignore Angular synchronization, this is a non-angular page
       return browser.ignoreSynchronization = true;
@@ -26,28 +26,32 @@
     it("Should apply correct background color", function () {
       // body background color
       expect(element(by.css("body")).getAttribute("style")).
-        to.eventually.equal("background: rgba(145, 145, 145, 0);");
+        to.eventually.equal("background: rgba(100, 100, 100, 0);");
     });
 
-    it("Should display background image", function () {
+    // ******
+    // TODO: Turn following tests on if a workaround is in place for testing the usage of rise-storage
+    // ******
+
+    xit("Should display background image", function () {
       // background image
       expect(element(by.id("background")).getAttribute("style")).
-        to.eventually.equal("background-image: url(http://s3.amazonaws.com/rise-common/images/logo-small.png);");
+        to.eventually.equal("background-image: url(https://storage.googleapis.com/risemedialibrary-dd474bee-b237-46e3-aa20-98e975679773/1_chopsticks.jpg);");
 
       // scale to fit class should be applied
-      expect(element(by.css(".scale-to-fit")).isPresent()).to.eventually.be.true;
+      expect(element(by.css(".scale-to-fit")).isPresent()).to.eventually.be.false;
 
       // correct positioning class
-      expect(element(by.css(".middle-center")).isPresent()).to.eventually.be.true;
+      expect(element(by.css(".bottom-right")).isPresent()).to.eventually.be.true;
     });
 
-    it("Should load and display video", function () {
+    xit("Should load and display video", function () {
       // source element should exist
       expect(element(by.tagName("source")).isPresent()).to.eventually.be.true;
 
       // source element should apply "src" attribute with correct value
       expect(element(by.css("source")).getAttribute("src")).
-        to.eventually.equal("https://s3.amazonaws.com/risecontentfiles/tests/a_RFID.webm");
+        to.eventually.equal("https://storage.googleapis.com/risemedialibrary-dd474bee-b237-46e3-aa20-98e975679773/videos%2Fa_food_show.webm");
 
       // video container should be visible
       expect(element(by.id("videoContainer")).isPresent()).to.eventually.be.true;
