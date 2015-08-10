179ee921e5a9f301fe23eed33e42910808d3844b
Stuart Lees <stuartlees@gmail.com>
2015-08-10 17:25:31 -0400

    Revising integration test coverage for Storage
    - Waiting for rise-storage-response to be fired before running initialization tests
    - Adding test for ensuring RiseVision.Video.onStorageInit is called on initial response
    - Adding test for ensuring RiseVision.Video.onStorageRefresh is called on response with `changed` attribute

--- test/data/storage.js
+++ test/data/storage.js
@@ -5,9 +5,9 @@
   window.gadget.settings = {
     "params": {},
     "additionalParams": {
-      "url": "https://storage.googleapis.com/risemedialibrary-b428b4e8-c8b9-41d5-8a10-b4193c789443/Widgets/videos/a_food_show.webm",
+      "url": "https://storage.googleapis.com/risemedialibrary-b428b4e8-c8b9-41d5-8a10-b4193c789443/Widgets%2Fvideos%2Fa_food_show.webm",
       "storage": {
-        "folder": "Widgets/",
+        "folder": "Widgets/videos/",
         "fileName": "a_food_show.webm",
         "companyId": "b428b4e8-c8b9-41d5-8a10-b4193c789443"
       },
