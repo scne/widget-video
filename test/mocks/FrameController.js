f5962a828559573ef9a1a66ee63608ac26234285
Stuart Lees <stuartlees@gmail.com>
2015-10-22 11:06:46 -0400

    Adding test case for fix to non-storage messaging
    - Added mock for `FrameController` module
    - Added non-storage data
    - Added non-storage integration test for showing the waiting message and not showing a message when player is ready

---
+++ test/mocks/FrameController.js
@@ -0,0 +1,51 @@
+(function (window) {
+  "use strict";
+
+  window.RiseVision.Common.Video.FrameController = function() {
+    return {
+      add: function(index) {
+        var frameContainer = this.getFrameContainer(index),
+          iframe = document.createElement("iframe");
+
+        frameContainer.appendChild(iframe);
+      },
+      createFramePlayer: function(index, params, files, skin, src) {
+      },
+      getFrameContainer: function(index) {
+        return document.getElementById("if_" + index);
+      },
+      getFrameObject: function(index) {
+        var frameContainer = this.getFrameContainer(index),
+          iframe;
+
+        iframe = frameContainer.querySelector("iframe");
+
+        if (iframe) {
+          return {
+            play: function () {},
+            pause: function () {},
+            remove: function () {}
+          };
+        }
+
+        return null;
+      },
+      remove: function(index, callback) {
+        var frameContainer = this.getFrameContainer(index),
+          frameObj = this.getFrameObject(index);
+
+        if (frameObj) {
+          frameObj.remove();
+        }
+
+        while (frameContainer.firstChild) {
+          frameContainer.removeChild(frameContainer.firstChild);
+        }
+
+        if (callback && typeof callback === "function") {
+          callback();
+        }
+      }
+    };
+  }
+})(window);
